16fb0eb86ee2e8fb4d167fd2476351c1f964d315
ian038 <ianphua3@gmail.com>
2022-01-31 14:16:15 -0500

    Added small card component to main page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import Footer from '../components/Footer'
+import SmallCard from '../components/SmallCard'
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -15,6 +16,16 @@ export default function Home({ exploreData, cardsData }) {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {exploreData?.map(item => (
+            <SmallCard 
+            key={item.img}
+              img={item.img} 
+              distance={item.distance} 
+              location={item.location} 
+            />
+          ))}
+          </div>
         </section>
       </main>
       <Footer />
