86d6b941f5e86d8ca8eb4e845c7354a2f7ef5dbc
ian038 <ianphua3@gmail.com>
2022-01-31 14:13:34 -0500

    WIP main page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 
-export default function Home() {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div>
       <Head>
@@ -11,11 +12,25 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
-      <main>
-        <h1>
-          Hello world
-        </h1>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
       </main>
+      <Footer />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(res => res.json())
+
+  const cardsData = await fetch('https://links.papareact.com/zp1').then(res => res.json())
+
+  return {
+    props: {
+      exploreData,
+      cardsData
+    }
+  }
 }
